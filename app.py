e5c13db484d0e5a13295b6e7ae938b1bbb274b2a
NektorHealth <85933978+NektorHealth@users.noreply.github.com>
2024-06-22 13:06:10 +0530

    Added CORS handling for frontend

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask , jsonify
+from flask_cors import CORS
 
 app=Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+CORS(app, resources={r"/api/*": {"origins":["https://voluble-taiyaki-30e7ef.netlify.app/", "http://localhost:3000/"]}})
+
+@app.route("/api/data", methods=["GET", "POST"])
 def get_data():
     data = {
         "name": "John Doe",
         "email": "john.doe@example.com",
         "phone": "123-456-7890"
     }
-    return jsonify(data)
+
+    response = jsonify(data)
+
+   # response.headers.add('Access-Control-Allow-Origin', 'https://voluble-taiyaki-30e7ef.netlify.app')  # Replace with your frontend domain
+    return response
 
 
 
